e2bbfd27814dd4535b72aedcacb296e2f20ca337
Simon Eriksson <Email>
2012-02-14 22:18:29 +0100

    Added validation on username, can only exist once

--- scrum_cards_server.js
+++ scrum_cards_server.js
@@ -1,6 +1,6 @@
 var app = require('express').createServer(),
     io = require('socket.io').listen(app),
-    sockDict = {},
+    sockDict = new Array();
     adminSocket = null;
 
 app.listen(8081);
@@ -8,12 +8,21 @@ app.listen(8081);
 io.sockets.on('connection',function(socket){
 
   socket.on('signIn',function(data,fn){
-    socket.broadcast.emit('reset');
-    sockDict[socket] = data.userName;
+
     if(process.argv.length <= 2){
       fn(false,'Not enough startup arguments when starting the server app');
       return;
     }
+
+    for(sock in sockDict){
+      var name = sockDict[sock];
+      if(name == data.userName){
+        fn(false,"Username taken!");
+        return;
+      }
+    }
+
+    sockDict[socket.id] = data.userName;
     if(data.userName == process.argv[2]){
       adminSocket = socket;
     }else{
@@ -35,19 +44,20 @@ io.sockets.on('connection',function(socket){
 
   socket.on('resetCmd',function(){
     console.log('resetCmd');
-    socket.broadcast.emit('reset');
+    if(sockDict == null) return;
+    socket.broadcast.emit('reset',{ 'userName' : sockDict[socket.id] });
   });
 
   socket.on('disconnect',function(){
     console.log('disconnect');
-    if(sockDict[socket] != null){
-      adminSocket.emit('clientDisconnect', {userName : sockDict[socket] });
-    }
-    sockDict[socket] = null;
-    if(socket == adminSocket){
+    if(adminSocket != null && socket == adminSocket){
       console.log('admin disconnected');
       adminSocket = null;
+    }else{
+      adminSocket.emit('clientDisconnect', {userName : sockDict[socket.id] });
     }
+    sockDict[socket.id] = null;
+
   });
 
 });
